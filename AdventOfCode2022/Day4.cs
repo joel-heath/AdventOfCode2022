d9c5689d1ab48b5acfc16cb9085c55becedb90b1
Joel Heath <joelheath@outlook.com>
2022-12-04 06:14:42 +0000

    Day 4 one-liners, albeit terrible ones but I'm not sure how to simplify this gargantuan ternary statement.

--- AdventOfCode2022/Day4.cs
+++ AdventOfCode2022/Day4.cs
@@ -20,87 +20,14 @@ internal class Day4 : IDay
         { "2-4,6-8\r\n2-3,4-5\r\n5-7,7-9\r\n2-8,3-7\r\n6-6,4-6\r\n2-6,4-8", "4" }
     };
 
-    static void AddToDict<TKey>(Dictionary<TKey,int> dict, TKey key, int value)
-    {
-        if (dict.TryGetValue(key, out int oldValue))
-        {
-            dict[key] = oldValue + value;
-        }
-        else
-        {
-            dict[key] = value;
-        }
-    }
-    static void AddToDict<TKey>(Dictionary<TKey, string> dict, TKey key, string value)
-    {
-        if (dict.TryGetValue(key, out string? oldValue))
-        {
-            dict[key] = oldValue + value;
-        }
-        else
-        {
-            dict[key] = value;
-        }
-    }
-
-    static string[] GetMatches(string input, string regex)
-    {
-        return new Regex(regex, RegexOptions.IgnoreCase).Matches(input).Skip(1).Select(m => m.Value).ToArray();
-    }
-
-
     public string SolvePart1(string input)
-    {
-        string[] lines = input.Split("\r\n");
-        int count = 0;
-
-        for (int i = 0; i < lines.Length; i++)
-        {
-            string[] ranges = lines[i].Split(",");
-            int[] range1 = ranges[0].Split("-").Select(int.Parse).ToArray();
-            int[] range2 = ranges[1].Split("-").Select(int.Parse).ToArray();
-
-            if ((range2[1] >= range1[1] && range2[0] <= range1[0]) || (range1[1] >= range2[1] && range1[0] <= range2[0]))
-            {
-                count++;
-            }
-
-        }
-
-        return $"{count}";
-
-    }
+    => $"{input.Split("\r\n").Select(l => l.Split(",")
+                .Select(s => s.Split("-").Select(int.Parse).ToArray()).ToArray()).ToArray()
+                .Select(l => ((l[1][1] >= l[0][1] && l[1][0] <= l[0][0]) || (l[0][1] >= l[1][1] && l[0][0] <= l[1][0]) ? 1 : 0)).Sum()}";
 
     public string SolvePart2(string input)
-    {
-        string[] lines = input.Split("\r\n");
-
-        int count = 0;
-
-        for (int i = 0; i < lines.Length; i++)
-        {
-            string[] ranges = lines[i].Split(",");
-            int[] bounds1 = ranges[0].Split("-").Select(int.Parse).ToArray();
-            int[] bounds2 = ranges[1].Split("-").Select(int.Parse).ToArray();
-
-            int[] range1 = new int[bounds1[1] - bounds1[0] + 1];
-            int[] range2 = new int[bounds2[1] - bounds2[0] + 1];
-
-            for (int j = bounds1[0]; j <= bounds1[1]; j++)
-            {
-                
-                range1[j - bounds1[0]] = j;
-            }
-
-            for (int j = bounds2[0]; j <= bounds2[1]; j++)
-            {
-                range2[j - bounds2[0]] = j;
-            }
-
-            if (range1.Intersect(range2).ToArray().Length > 0) count++;
-
-        }
-
-        return $"{count}";
-    }
+    => $"{input.Split("\r\n").Select(l => l.Split(",")
+            .Select(s => s.Split("-").Select(int.Parse).ToArray())
+            .Select(s => Enumerable.Range(s[0], s[1] - s[0] + 1).ToArray()).ToArray())
+            .Select(l => l[0].Intersect(l[1]).Any() ? 1 : 0).Sum()}";
 }
