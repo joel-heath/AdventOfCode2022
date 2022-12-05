b314f5eaeace67b53d6e925318274ff183cfe85e
Joel Heath <joelheath@outlook.com>
2022-12-05 07:07:39 +0000

    Reduced the code to as close as I can probably get to one-liners—a massive ParseInput() function and a for loop with two lines inside. Close enough.

--- AdventOfCode2022/Day5.cs
+++ AdventOfCode2022/Day5.cs
@@ -19,16 +19,8 @@ internal partial class Day5 : IDay
         { "    [D]    \r\n[N] [C]    \r\n[Z] [M] [P]\r\n 1   2   3 \r\n\r\nmove 1 from 2 to 1\r\nmove 3 from 1 to 3\r\nmove 2 from 2 to 1\r\nmove 1 from 1 to 2", "MCD" }
     };
 
-    static string[] GetMatches(string input, string regex)
+    static (Stack<char>[], int) ParseInput(string[] lines)
     {
-        return new Regex(regex, RegexOptions.IgnoreCase).Matches(input).Skip(1).Select(m => m.Value).ToArray();
-    }
-
-
-    public string SolvePart1(string input)
-    {
-        string[] lines = input.Split("\r\n");
-
         int intialCrates = -1;
         for (int i = 0; i < lines.Length && intialCrates == -1; i++)
         {
@@ -43,60 +35,7 @@ internal partial class Day5 : IDay
             if (c != '[' && c != ' ' && c != ']') crateCount++;
         }
 
-        Stack<char>[] parts = new Stack<char>[crateCount].Select(s => new Stack<char>()).ToArray();
-
-        for (int i = intialCrates-1; i >= 0; i--)
-        {
-            string line = lines[i];
-
-            for (int j = 0, k = 1; j < crateCount; j++, k += 4)
-            {
-                if (line[k] == ' ') continue;
-                parts[j].Push(line[k]);
-            }
-        }
-
-
-        for (int i = intialCrates+2; i < lines.Length; i++)
-        {
-            string line = lines[i];
-            int[] nums = MoveFromTo().Matches(lines[i])[0].Groups.Cast<Group>().Skip(1).Select(g => int.Parse(g.Value)).ToArray();
-
-            for (int j = 0; j < nums[0]; j++)
-            {
-                parts[nums[2]-1].Push( parts[nums[1]-1].Pop() );
-            }
-        }
-
-        string result = string.Empty;
-
-        foreach (Stack<char> stack in parts)
-        {
-            result += stack.Pop();
-        }
-
-        return $"{result}";
-    }
-
-    public string SolvePart2(string input)
-    {
-        string[] lines = input.Split("\r\n");
-
-        int intialCrates = -1;
-        for (int i = 0; i < lines.Length && intialCrates == -1; i++)
-        {
-            if (lines[i].Length < 2 || lines[i][0] == '[') continue;
-            if (lines[i][1] == '1') intialCrates = i;
-        }
-
-        int crateCount = 0;
-        for (int i = 0; i < lines[intialCrates].Length; i++)
-        {
-            char c = lines[intialCrates][i];
-            if (c != '[' && c != ' ' && c != ']') crateCount++;
-        }
-
-        Stack<char>[] parts = new Stack<char>[crateCount].Select(s => new Stack<char>()).ToArray();
+        Stack<char>[] crates = new Stack<char>[crateCount].Select(s => new Stack<char>()).ToArray();
 
         for (int i = intialCrates - 1; i >= 0; i--)
         {
@@ -105,36 +44,39 @@ internal partial class Day5 : IDay
             for (int j = 0, k = 1; j < crateCount; j++, k += 4)
             {
                 if (line[k] == ' ') continue;
-                parts[j].Push(line[k]);
+                crates[j].Push(line[k]);
             }
         }
 
+        return (crates, intialCrates+2);
+    }
 
-        for (int i = intialCrates + 2; i < lines.Length; i++)
+    public string SolvePart1(string input)
+    {
+        string[] lines = input.Split("\r\n");
+        (Stack<char>[] crates, int instructionLine) = ParseInput(lines);
+
+        for (int i = instructionLine; i < lines.Length; i++)
         {
             int[] nums = MoveFromTo().Matches(lines[i])[0].Groups.Cast<Group>().Skip(1).Select(g => int.Parse(g.Value)).ToArray();
-
-            List<char> temp = new();
-
-            for (int j = 0; j < nums[0]; j++)
-            {
-                temp.Add(parts[nums[1] - 1].Pop());
-            }
-
-            for (int j = temp.Count - 1; j >= 0; j--)
-            {
-                parts[nums[2] - 1].Push(temp[j]);
-            }
+            Enumerable.Range(0, nums[0]).Select(n => crates[nums[1] - 1].Pop()).ToList().ForEach(c => crates[nums[2] - 1].Push(c));
         }
 
-        string result = string.Empty;
+        return new string(crates.Select(s => s.Peek()).ToArray());
+    }
 
-        foreach (Stack<char> stack in parts)
+    public string SolvePart2(string input)
+    {
+        string[] lines = input.Split("\r\n");
+        (Stack<char>[] crates, int instructionLine) = ParseInput(lines);
+
+        for (int i = instructionLine; i < lines.Length; i++)
         {
-            result += stack.Pop();
+            int[] nums = MoveFromTo().Matches(lines[i])[0].Groups.Cast<Group>().Skip(1).Select(g => int.Parse(g.Value)).ToArray();
+            Enumerable.Range(0, nums[0]).Select(n => crates[nums[1] - 1].Pop()).Reverse().ToList().ForEach(c => crates[nums[2] - 1].Push(c));
         }
 
-        return $"{result}";
+        return new string(crates.Select(s => s.Peek()).ToArray());
     }
 
     [GeneratedRegex("move (\\d+) from (\\d+) to (\\d+)", RegexOptions.IgnoreCase, "en-GB")]
