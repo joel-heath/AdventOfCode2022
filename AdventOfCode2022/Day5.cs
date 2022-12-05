cdfc4aeca085ab8733e24f4d995cf5a0ca33daf2
Joel Heath <joelheath@outlook.com>
2022-12-05 06:23:34 +0000

    Day 5 completed Part 1 38:10, Part 2 40:38

--- AdventOfCode2022/Day5.cs
+++ AdventOfCode2022/Day5.cs
@@ -7,40 +7,17 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode2022;
-internal class Day5 : IDay
+internal partial class Day5 : IDay
 {
     public int Day => 5;
     public Dictionary<string, string> UnitTestsP1 => new()
     {
-        { "TestInput", "Output" }
+        { "    [D]    \r\n[N] [C]    \r\n[Z] [M] [P]\r\n 1   2   3 \r\n\r\nmove 1 from 2 to 1\r\nmove 3 from 1 to 3\r\nmove 2 from 2 to 1\r\nmove 1 from 1 to 2", "CMZ" }
     };
     public Dictionary<string, string> UnitTestsP2 => new()
     {
-        { "TestInput", "Output" }
+        { "    [D]    \r\n[N] [C]    \r\n[Z] [M] [P]\r\n 1   2   3 \r\n\r\nmove 1 from 2 to 1\r\nmove 3 from 1 to 3\r\nmove 2 from 2 to 1\r\nmove 1 from 1 to 2", "MCD" }
     };
-
-    static void AddToDict<TKey>(Dictionary<TKey, int> dict, TKey key, int value)
-    {
-        if (dict.TryGetValue(key, out int oldValue))
-        {
-            dict[key] = oldValue + value;
-        }
-        else
-        {
-            dict[key] = value;
-        }
-    }
-    static void AddToDict<TKey>(Dictionary<TKey, string> dict, TKey key, string value)
-    {
-        if (dict.TryGetValue(key, out string? oldValue))
-        {
-            dict[key] = oldValue + value;
-        }
-        else
-        {
-            dict[key] = value;
-        }
-    }
 
     static string[] GetMatches(string input, string regex)
     {
@@ -52,52 +29,114 @@ internal class Day5 : IDay
     {
         string[] lines = input.Split("\r\n");
 
-        Dictionary<string, int> result = new();
-        int count = 0;
-
-        for (int i = 0; i < lines.Length; i++)
+        int intialCrates = -1;
+        for (int i = 0; i < lines.Length && intialCrates == -1; i++)
         {
-            string[] words = lines[i].Split(" ").Select(s => s.Trim(' ')).ToArray();
+            if (lines[i].Length < 2 || lines[i][0] == '[') continue;
+            if (lines[i][1] == '1') intialCrates = i;
+        }
 
+        int crateCount = 0;
+        for (int i = 0; i < lines[intialCrates].Length; i++)
+        {
+            char c = lines[intialCrates][i];
+            if (c != '[' && c != ' ' && c != ']') crateCount++;
+        }
 
+        Stack<char>[] parts = new Stack<char>[crateCount].Select(s => new Stack<char>()).ToArray();
 
-            for (int j = 0; j < words.Length; j++)
+        for (int i = intialCrates-1; i >= 0; i--)
+        {
+            string line = lines[i];
+
+            for (int j = 0, k = 1; j < crateCount; j++, k += 4)
             {
-                string word = words[j];
-                if (word == "") continue;
-
-
+                if (line[k] == ' ') continue;
+                parts[j].Push(line[k]);
             }
         }
 
 
-
-        // OR REGEX ROUTE
-        Regex r = new("(\\d+),(\\d+) => (\\d+),(\\d+)", RegexOptions.IgnoreCase);
-        MatchCollection matches = r.Matches(input);
-
-        foreach (Match m in matches)
+        for (int i = intialCrates+2; i < lines.Length; i++)
         {
-            GroupCollection g = m.Groups;
+            string line = lines[i];
+            int[] nums = MoveFromTo().Matches(lines[i])[0].Groups.Cast<Group>().Skip(1).Select(g => int.Parse(g.Value)).ToArray();
 
-            //g[0] is the overarching capture of the whole thing
-            // skip it with index at 1
-            for (int gi = 1; gi < g.Count; gi++)
+            for (int j = 0; j < nums[0]; j++)
             {
-                Console.Write($"{g[gi].Value} ");
+                parts[nums[2]-1].Push( parts[nums[1]-1].Pop() );
             }
-
-            Console.WriteLine();
         }
 
+        string result = string.Empty;
 
+        foreach (Stack<char> stack in parts)
+        {
+            result += stack.Pop();
+        }
 
-
-        return $"{string.Empty}";
+        return $"{result}";
     }
 
     public string SolvePart2(string input)
     {
-        return $"{string.Empty}";
+        string[] lines = input.Split("\r\n");
+
+        int intialCrates = -1;
+        for (int i = 0; i < lines.Length && intialCrates == -1; i++)
+        {
+            if (lines[i].Length < 2 || lines[i][0] == '[') continue;
+            if (lines[i][1] == '1') intialCrates = i;
+        }
+
+        int crateCount = 0;
+        for (int i = 0; i < lines[intialCrates].Length; i++)
+        {
+            char c = lines[intialCrates][i];
+            if (c != '[' && c != ' ' && c != ']') crateCount++;
+        }
+
+        Stack<char>[] parts = new Stack<char>[crateCount].Select(s => new Stack<char>()).ToArray();
+
+        for (int i = intialCrates - 1; i >= 0; i--)
+        {
+            string line = lines[i];
+
+            for (int j = 0, k = 1; j < crateCount; j++, k += 4)
+            {
+                if (line[k] == ' ') continue;
+                parts[j].Push(line[k]);
+            }
+        }
+
+
+        for (int i = intialCrates + 2; i < lines.Length; i++)
+        {
+            int[] nums = MoveFromTo().Matches(lines[i])[0].Groups.Cast<Group>().Skip(1).Select(g => int.Parse(g.Value)).ToArray();
+
+            List<char> temp = new();
+
+            for (int j = 0; j < nums[0]; j++)
+            {
+                temp.Add(parts[nums[1] - 1].Pop());
+            }
+
+            for (int j = temp.Count - 1; j >= 0; j--)
+            {
+                parts[nums[2] - 1].Push(temp[j]);
+            }
+        }
+
+        string result = string.Empty;
+
+        foreach (Stack<char> stack in parts)
+        {
+            result += stack.Pop();
+        }
+
+        return $"{result}";
     }
+
+    [GeneratedRegex("move (\\d+) from (\\d+) to (\\d+)", RegexOptions.IgnoreCase, "en-GB")]
+    private static partial Regex MoveFromTo();
 }
