1b3a7814e2b896f01005e73b39dd5f451280bd77
Joel Heath <joelheath@outlook.com>
2022-12-12 09:18:50 +0000

    Still more cleaning to do but day 12 neatened up a bit

--- AdventOfCode2022/Day12.cs
+++ AdventOfCode2022/Day12.cs
@@ -33,11 +33,17 @@ internal class Day12 : IDay
 
         return (To2D(map), (startY, startX), (endY, endX));
     }
+    static T[,] To2D<T>(T[][] source)
+    {
+        var result = new T[source.Length, source[0].Length];
+        Enumerable.Range(0, source.Length).ToList().ForEach(r => Enumerable.Range(0, source[0].Length).ToList().ForEach(c => result[r, c] = source[r][c]));
+        return result;
+    }
 
     public class Node
     {
         public Node? p; // parent
-        public Point s;
+        public Point s; // self
         public int g; // distance from origin
 
         public Node(Point self, Node? parent)
@@ -45,12 +51,10 @@ internal class Day12 : IDay
             this.p = parent;
             this.s = self;
             this.g = Equals(parent, null) ? 0 : p.g + 1;
-            //this.h = self.DistanceFrom(end);
-            //this.f = this.g + this.h;
         }
 
         public Node Add(Point b) => new ((s.y + b.y, s.x + b.x), this);
-        public static bool operator ==(Node a, Point b) => a.s.y == b.y && a.s.x == b.x;
+        public static bool operator ==(Node a, Point? b) => b == null ? false : a.s.y == b.y && a.s.x == b.x;
         public static bool operator !=(Node a, Point b) => a.s.y != b.y || a.s.x != b.x;
     }
 
@@ -64,39 +68,19 @@ internal class Day12 : IDay
             this.y = y;
             this.x = x;
         }
-
-        public int DistanceFrom(Point p)
-        => Math.Abs(p.y - this.y) + Math.Abs(p.x - this.x);
-
-        public int DistanceFromO()
-        => this.y + this.x;
-
-        public static Point operator +(Point a, Point b) => (a.y + b.y, a.x + b.x);
         
-        public static implicit operator int(Point a) => a.y + a.x;
-
-        public static implicit operator Point(Tuple<int,int> coords) => new(coords.Item1,coords.Item2);
         public static implicit operator Point(ValueTuple<int, int> coords) => new(coords.Item1, coords.Item2);
         public static explicit operator Point(Node n) => n.s;
     }
 
-    static bool IsP1Valid(Point s, Point d, int[,] map)
-    {
-        int rows = map.GetLength(0);
-        int cols = map.GetLength(1);
-
-        if (d.y < 0 || d.y >= rows || d.x < 0 || d.x >= cols) return false;
-
-        return map[d.y, d.x] - map[s.y, s.x] <= 1;
-    }
     static bool IsValid(Point s, Point d, int[,] map)
     {
         int rows = map.GetLength(0);
         int cols = map.GetLength(1);
 
-        if (d.y < 0 || d.y >= rows || d.x < 0 || d.x >= cols) return false;
+        if (d.y < 0 || d.y >= rows || d.x < 0 || d.x >= cols || s.y < 0 || s.y >= rows || s.x < 0 || s.x >= cols) return false;
 
-        return map[s.y, s.x] - map[d.y, d.x] <= 1;
+        return map[d.y, d.x] - map[s.y, s.x] <= 1;
     }
 
     static int IndexOf(List<Node> list, Point p)
@@ -108,87 +92,14 @@ internal class Day12 : IDay
         }
         return -1;
     }
-    /*
-    static int PathFind(Point start, Point end, int[,] map)
+    
+    static (List<Node>, Point) PathFind(Point start, int[,] map, Point? end = null)
     {
         int ROW = map.GetLength(0);
         int COL = map.GetLength(1);
 
-        List<Node> open = new () { new Node(start, null, end) };
+        List<Node> open = new () { new (start, null) };
         List<Node> closed = new ();
-
-        while (IndexOf(closed, end) == -1 && open.Count > 0)
-        {
-            Point[] vectors = { (1, 0), (-1, 0), (0, 1), (0, -1) };
-
-            Node currentNode = open.MinBy(n => n.g)!;
-            open.Remove(currentNode);
-            closed.Add(currentNode);
-
-            if (currentNode == end) { break; }
-
-            foreach (Point vector in vectors)
-            {
-                Node newNode = currentNode.Add(vector);
-
-                if (IndexOf(closed, (Point)newNode) != -1) { continue; }
-                
-                if (!IsValid((Point)currentNode, (Point)newNode, map)) { continue; }
-
-                if (IndexOf(open, (Point)newNode) == -1)
-                {
-                    open.Add(newNode);
-                }
-                else
-                {
-                    if (newNode.g < open[IndexOf(open, (Point)newNode)].g)
-                    {
-                        open[IndexOf(open, (Point)newNode)] = newNode;
-                    }
-                }
-            }
-        }
-
-        if (IndexOf(closed, end) == -1) return -1;
-
-        Node curr = closed[IndexOf(closed, end)];
-        int count = 0;
-
-        while (!Equals(curr.p, null))
-        {
-            curr = curr.p; count++;
-        }
-
-        return count;
-    }*/
-
-    static T[,] To2D<T>(T[][] source)
-    {
-        try
-        {
-            int FirstDim = source.Length;
-            int SecondDim = source.GroupBy(row => row.Length).Single().Key; // throws InvalidOperationException if source is not rectangular
-
-            var result = new T[FirstDim, SecondDim];
-            for (int i = 0; i < FirstDim; ++i)
-                for (int j = 0; j < SecondDim; ++j)
-                    result[i, j] = source[i][j];
-
-            return result;
-        }
-        catch (InvalidOperationException)
-        {
-            throw new InvalidOperationException("The given jagged array is not rectangular.");
-        }
-    }
-
-    static int Dijkstras(Point start, int[,] map)
-    {
-        int ROW = map.GetLength(0);
-        int COL = map.GetLength(1);
-
-        List<Node> open = new() { new (start, null) };
-        List<Node> closed = new();
 
         Node currentNode = new (start, null);
 
@@ -196,12 +107,12 @@ internal class Day12 : IDay
         {
             Point[] vectors = { (1, 0), (-1, 0), (0, 1), (0, -1) };
 
-            //Console.WriteLine(open.Count);
             currentNode = open.MinBy(n => n.g)!;
             open.Remove(currentNode);
             closed.Add(currentNode);
 
-            if (map[currentNode.s.y, currentNode.s.x] == 0) { break; }
+            if (end == null && map[currentNode.s.y, currentNode.s.x] == 0) { break; }
+            else if (currentNode == end) { break; }
             
             foreach (Point vector in vectors)
             {
@@ -209,7 +120,8 @@ internal class Day12 : IDay
 
                 if (IndexOf(closed, (Point)newNode) != -1) { continue; }
 
-                if (!IsValid((Point)currentNode, (Point)newNode, map)) { continue; }
+                if (end != null) { if (!IsValid((Point)currentNode, (Point)newNode, map)) { continue; } }
+                else             { if (!IsValid((Point)newNode, (Point)currentNode, map)) { continue; } }
 
                 if (IndexOf(open, (Point)newNode) == -1)
                 {
@@ -225,39 +137,22 @@ internal class Day12 : IDay
             }
         }
 
-        if (IndexOf(closed, currentNode.s) == -1) return -1;
-
-        Node curr = closed[IndexOf(closed, currentNode.s)];
-        int count = 0;
-
-        while (!Equals(curr.p, null))
-        {
-            curr = curr.p; count++;
-        }
-
-        return count;
+        return (closed, currentNode.s);
     }
 
     public string SolvePart1(string input)
     {
-        /*
         (int[,] map, Point start, Point end) = ParseInput(input);
+        (List<Node> closed, _)= PathFind(start, map, end);
 
-        int distance = PathFind(start, end, map);
-
-        return $"{distance}";
-        */
-
-        return "lol fail";
+        return $"{closed[IndexOf(closed, end)].g}";
     }
 
     public string SolvePart2(string input)
     {
-        (int[,] map, _, Point E) = ParseInput(input);
-        int distance = Dijkstras(E, map);
+        (int[,] map, _, Point end) = ParseInput(input);
+        (List<Node> closed, Point start) = PathFind(end, map);
 
-        return $"{distance}";
-
-        // 173 too low
+        return $"{closed[IndexOf(closed, start)].g}";
     }
 }
