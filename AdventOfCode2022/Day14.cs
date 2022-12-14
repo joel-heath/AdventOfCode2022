121a70eb566ffa1c3504d089660a18ec35e1ffc5
Joel Heath <joelheath@outlook.com>
2022-12-14 06:03:41 +0000

    Day 14 Part 1 done, VERY SLOW

--- AdventOfCode2022/Day14.cs
+++ AdventOfCode2022/Day14.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+using System.Security;
+using System.Numerics;
+using System.Security.Cryptography;
 
 namespace AdventOfCode2022;
 internal class Day14 : IDay
@@ -11,13 +16,11 @@ internal class Day14 : IDay
     public int Day => 14;
     public Dictionary<string, string> UnitTestsP1 => new()
     {
-        { "TestInput", "Output" },
-        { "Input2", "Output" },
+        { "498,4 -> 498,6 -> 496,6\r\n503,4 -> 502,4 -> 502,9 -> 494,9", "24" }
     };
     public Dictionary<string, string> UnitTestsP2 => new()
     {
-        { "TestInput", "Output" },
-        { "Input2", "Output" },
+        { "TestInput", "Output" }
     };
 
     static void AddToDict<TKey>(Dictionary<TKey, int> dict, TKey key, int value)
@@ -43,52 +46,210 @@ internal class Day14 : IDay
         }
     }
 
-    static T?[] GetNeighbours<T>(T[,] grid, int y, int x, bool diagonals = false)
+    
+
+    enum Material { Air, Rock, RestedSand, Sand }
+
+    static (Dictionary<Point, Material>, int) ParseInput(string input)
     {
-        var coordsToCheck = new (int, int)[]
-        {
-            (y - 1, x), // Up
-            (y, x + 1), // Right
-            (y + 1, x), // Down
-            (y, x - 1), // Left
-        };
+        int abyssLevel = 0;
+        string[] lines = input.Split("\r\n");
 
-        if (diagonals)
+        Dictionary<Point, Material> grid = new Dictionary<Point, Material>();
+
+        for (int i = 0; i < lines.Length; i++)
         {
-            coordsToCheck = coordsToCheck.Concat(new (int, int)[]
+            string[] line = lines[i].Split(" -> ");
+            
+            Point? lastCoord = null;
+            for (int j = 0; j < line.Length; j++)
             {
-                (y - 1, x + 1), // Up-Right
-                (y + 1, x + 1), // Down-Right
-                (y + 1, x - 1), // Down-Left
-                (y - 1, x - 1), // Up-Left
+                IEnumerable<int> coord = line[j].Split(',').Select(int.Parse);
+                Point here = (coord.First(), coord.Last());
 
-            }).ToArray();
+                if (coord.Last() > abyssLevel) abyssLevel = coord.Last();
+
+                if (lastCoord == null)
+                {
+                    grid.Add(here, Material.Rock);
+                }
+                else
+                {
+                    lastCoord.PointsTo(here, true).ToList().ForEach(p => grid.Add(p, Material.Rock));
+                }
+
+                lastCoord = here;
+            }
         }
 
-        return coordsToCheck.Select(c => y < grid.GetLength(0) && x < grid.GetLength(1) ? grid[c.Item1, c.Item2] : default).ToArray();
+        return (grid, abyssLevel + 1);
+    }
+
+    static bool PositionBlocked(Dictionary<Point, Material> grid, Point point)
+    {
+        foreach (KeyValuePair<Point, Material> pair in grid)
+        {
+            if (pair.Key.X == point.X && pair.Key.Y == point.Y) return true;
+        }
+        return false;
+    }
+
+    static Point? FlowSand(Dictionary<Point, Material> grid, Point p)
+    {
+        var vectors = new Point[]
+        {
+            ( 0, 1), // Down
+            (-1, 1), // Down-Left
+            ( 1, 1), // Down-Right
+        };
+
+        foreach (var coord in vectors)
+        {
+            Point newPoint = (p.X + coord.X, p.Y + coord.Y);
+
+            if (PositionBlocked(grid, newPoint))
+            {
+                continue;
+            }
+            else
+            {
+                return newPoint;
+            }
+        }
+
+        return null;
+    }
+
+    static void DrawGrid(Dictionary<Point, Material> grid)
+    {
+        foreach (Point p in grid.Keys)
+        {
+            Console.SetCursorPosition(p.X - 450, p.Y);
+            
+            if (grid[p] == Material.Rock)
+            {
+                Console.Write('#');
+            }
+            else if (grid[p] == Material.RestedSand)
+            {
+                Console.Write('o');
+            }
+        }
+
     }
 
     public string SolvePart1(string input)
     {
-        string[] lines = input.Split("\r\n");
-
+        (var grid, int abyssLevel) = ParseInput(input);
+        
+        Console.WriteLine("Parsed!");
+        
         int count = 0;
-
-        for (int i = 0; i < lines.Length; i++)
+        bool reachedAbyss = false;
+        while (!reachedAbyss)
         {
-            string line = lines[i].Trim(' ');
-
-
-
-
-            for (int j = 0; j < line.Length; j++)
+            bool rested = false;
+            Point pos = (500, 1);
+            while (!rested)
             {
-                count += j;
+                Point? newPos = FlowSand(grid, pos);
+
+                //Console.WriteLine(newPos);
+
+                if (newPos == null)
+                {
+                    rested = true;
+                    count++;
+                    grid.Add(pos, Material.RestedSand);
+                }
+                else
+                {
+                    pos = newPos;
+                    if (newPos.Y > abyssLevel)
+                    {
+                        reachedAbyss = true;
+                        //count--;
+                        break;
+                    }
+                }
             }
         }
 
+        return $"{count}";
+    }
 
-        return $"{string.Empty}";
+
+
+    class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point[] PointsTo(Point p, bool inclusive = false)
+        {
+            if (X == p.X)
+            {
+                int big = Math.Max(Y, p.Y);
+                int small = Math.Min(Y, p.Y);
+                Point[] points;
+
+                if (!inclusive)
+                {
+                    points = new Point[big - small];
+                    for (int i = 0; i < big - small; i++)
+                    {
+                        points[i] = (X, small + i);
+                    }
+                }
+                else
+                {
+                    points = new Point[big - small + 1];
+                    for (int i = 0; i <= big - small; i++)
+                    {
+                        points[i] = (X, small + i);
+                    }
+                }
+                return points;
+            }
+            else if (Y == p.Y)
+            {
+                int big = Math.Max(X, p.X);
+                int small = Math.Min(X, p.X);
+                Point[] points;
+
+                if (!inclusive)
+                {
+                    points = new Point[big - small];
+                    for (int i = 0; i < big - small; i++)
+                    {
+                        points[i] = (small + i, Y);
+                    }
+                }
+                else
+                {
+                    points = new Point[big - small + 1];
+                    for (int i = 0; i <= big - small; i++)
+                    {
+                        points[i] = (small + i, Y);
+                    }
+                }
+                return points;
+            }
+            else { throw new Exception("Cannot build straight line between points"); }
+        }
+
+        public override string ToString() => $"({X}, {Y})";
+        public static implicit operator Point(ValueTuple<int, int> tuple) => new(tuple.Item1, tuple.Item2);
+
+        public static Point operator +(Point a, Point b) => new(a.X + b.X, a.Y + b.Y);
+        //public static bool operator ==(Point? a, Point? b) => (a.Equals(null) && b.Equals(null)) || ( !b.Equals(null) && a.X == b.X && a.Y == b.Y);
+        //public static bool operator !=(Point? a, Point? b) => a.X != b.X || a.Y != b.Y;
     }
 
     public string SolvePart2(string input)
