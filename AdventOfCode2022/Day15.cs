7d170463af7d81684fc3da60e0c48e3164486d21
Joel Heath <joelheath@outlook.com>
2022-12-15 20:25:26 +0000

    Day 15 DONE

--- AdventOfCode2022/Day15.cs
+++ AdventOfCode2022/Day15.cs
@@ -11,6 +11,7 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Text.RegularExpressions;
 using System.Linq.Expressions;
+using System.Text.Unicode;
 
 namespace AdventOfCode2022;
 internal partial class Day15 : IDay
@@ -22,7 +23,7 @@ internal partial class Day15 : IDay
     };
     public Dictionary<string, string> UnitTestsP2 => new()
     {
-        { "TestInput", "Output" }
+        { "Sensor at x=2, y=18: closest beacon is at x=-2, y=15\r\nSensor at x=9, y=16: closest beacon is at x=10, y=16\r\nSensor at x=13, y=2: closest beacon is at x=15, y=3\r\nSensor at x=12, y=14: closest beacon is at x=10, y=16\r\nSensor at x=10, y=20: closest beacon is at x=10, y=16\r\nSensor at x=14, y=17: closest beacon is at x=10, y=16\r\nSensor at x=8, y=7: closest beacon is at x=2, y=10\r\nSensor at x=2, y=0: closest beacon is at x=2, y=10\r\nSensor at x=0, y=11: closest beacon is at x=2, y=10\r\nSensor at x=20, y=14: closest beacon is at x=25, y=17\r\nSensor at x=17, y=20: closest beacon is at x=21, y=22\r\nSensor at x=16, y=7: closest beacon is at x=15, y=3\r\nSensor at x=14, y=3: closest beacon is at x=15, y=3\r\nSensor at x=20, y=1: closest beacon is at x=15, y=3", "56000011" }
     };
 
     static bool AddToGrid(Dictionary<int, char> grid, int x, char c)
@@ -37,8 +38,6 @@ internal partial class Day15 : IDay
     static Dictionary<int, char> ParseInput(string input, int line)
     {
         Dictionary<int, char> grid = new();
-
-        //Regex r = InputParse();
 
         string[] lines = input.Split("\r\n");
         for (int i = 0; i < lines.Length; i++)
@@ -56,12 +55,7 @@ internal partial class Day15 : IDay
 
             if (-distance <= line - nums[1] && line - nums[1] <= distance)
             {
-                int count = distance - (line - nums[1]); // num of # either side of midpoint
-
-                if (count > distance) // Triangle goes back down - diamond shape
-                {                   // treat line as if it were opposite side of triangle
-                    count = distance - (line + 2 * (nums[1] - line) - nums[1]);
-                }
+                int count = distance - Math.Abs((line - nums[1])); // num of # either side of midpoint
 
                 for (int j = -count; j <= count; j++)
                 {
@@ -70,42 +64,109 @@ internal partial class Day15 : IDay
             }
         }
 
-
         return grid;
-    }
-
-    static void RenderLine(Dictionary<int, char> grid)
-    {
-        foreach (var kvp in grid)
-        {
-            Console.CursorLeft = kvp.Key+10;
-            Console.Write(kvp.Value);
-        }
     }
 
     public string SolvePart1(string input)
     {
-        const int YLEVEL = 2000000;
+        int YLEVEL = UnitTestsP1.ContainsKey(input) ? 10 : 2000000;
         var grid = ParseInput(input, YLEVEL);
-        Console.WriteLine("Input Parsed!");
-        
-        //RenderLine(grid);
-
-        /*
-        Console.ReadKey(true);
-        Console.Clear();
-        DrawGrid(grid);
-
-        Console.ReadKey(true);
-        */
-
 
         return $"{grid.Where(kvp => kvp.Value != 'B').Count()}";
     }
 
+    static void RangeCombiner((int Start, int End)?[] ranges)
+    {
+        for (int i = 0; i < ranges.Length; i++)
+        {
+            if (ranges[i] == null) continue;
+
+            for (int j = 0; j < ranges.Length; j++)
+            {
+                if (ranges[j] == null) continue;
+                if (i == j) continue;
+
+                int startLow = Math.Min(ranges[i]!.Value.Start, ranges[j]!.Value.Start);
+                int startHigh = Math.Max(ranges[i]!.Value.Start, ranges[j]!.Value.Start);
+                int endLow = Math.Min(ranges[i]!.Value.End, ranges[j]!.Value.End);
+                int endHigh = Math.Max(ranges[i]!.Value.End, ranges[j]!.Value.End);
+
+                if (startHigh - 1 <= endLow)
+                {
+                    ranges[i] = new(startLow, endHigh);
+                    ranges[j] = null;
+                    i = 0;
+                    j = 0;
+                }
+            }
+        }
+    }
+
+    static (int y, (int, int)[] x) FindRow(Dictionary<Point, int> sensors, int MAX)
+    {
+        for (int r = 0; r <= MAX; r++)
+        {
+            List<(int, int)?> ranges = new();
+
+            foreach (var sensor in sensors)
+            {
+                var manhattenDistance = sensor.Value;
+                var distanceFromCentre = Math.Abs(sensor.Key.Y - r);
+
+                if (distanceFromCentre <= manhattenDistance)
+                {
+                    var min = sensor.Key.X - (manhattenDistance - distanceFromCentre);
+                    var max = sensor.Key.X + (manhattenDistance - distanceFromCentre);
+                    (int, int) sensorRange = (Math.Max(0, min), Math.Min(max, MAX));
+                    
+                    
+                    ranges.Add(sensorRange);
+                }
+            }
+
+            var range = ranges.ToArray();
+            RangeCombiner(range);
+
+            (int, int)[] final = range.Where(r => r != null).Select(r => r!.Value).ToArray();
+
+
+            if (final.Length > 1)
+            {
+                return (r, final);
+            }
+        }
+
+        throw new("Cannot find row with no beacon");
+    }
+
+    static Dictionary<Point, int> ParseInput2(string input)
+    {
+        Dictionary<Point, int> sensors = new();
+
+        Regex r = InputParse();
+
+        string[] lines = input.Split("\r\n");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            int[] nums = r.Match(lines[i]).Groups.Cast<Group>().Skip(1).Select(c => int.Parse(c.Value)).ToArray();
+
+            int distance = Math.Abs(nums[2] - nums[0]) + Math.Abs(nums[3] - nums[1]);
+
+            sensors[(nums[0], nums[1])] = distance;
+        }
+
+        return sensors;
+    }
+
     public string SolvePart2(string input)
     {
-        return string.Empty;
+        int MAX = UnitTestsP2.ContainsKey(input) ? 20 : 4000000;
+        Dictionary<Point, int> sensors = ParseInput2(input);
+
+        (int y, (int, int)[] xVals) = FindRow(sensors, MAX);
+        long x = xVals.OrderBy(r => r.Item1).First().Item2 + 1;
+
+        return $"{x * 4000000 + y}";
     }
 
     [GeneratedRegex(@"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")]
