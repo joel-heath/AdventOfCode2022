6788b8ff9eb26e44bc7e1175e2487dcbc6628f38
Joel Heath <joelheath@outlook.com>
2022-12-16 17:16:51 +0000

    Day 16 part 1 very slow

--- AdventOfCode2022/Day16.cs
+++ AdventOfCode2022/Day16.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Net;
+using System.Xml.Linq;
+using System.ComponentModel;
+using System.Xml.Schema;
+using System.Reflection.Metadata.Ecma335;
 
 namespace AdventOfCode2022;
 internal partial class Day16 : IDay
@@ -16,7 +21,7 @@ internal partial class Day16 : IDay
     };
     public Dictionary<string, string> UnitTestsP2 => new()
     {
-        { "TestInput", "Output" }
+        { "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\r\nValve BB has flow rate=13; tunnels lead to valves CC, AA\r\nValve CC has flow rate=2; tunnels lead to valves DD, BB\r\nValve DD has flow rate=20; tunnels lead to valves CC, AA, EE\r\nValve EE has flow rate=3; tunnels lead to valves FF, DD\r\nValve FF has flow rate=0; tunnels lead to valves EE, GG\r\nValve GG has flow rate=0; tunnels lead to valves FF, HH\r\nValve HH has flow rate=22; tunnel leads to valve GG\r\nValve II has flow rate=0; tunnels lead to valves AA, JJ\r\nValve JJ has flow rate=21; tunnel leads to valve II", "1707" }
     };
 
     class Valve
@@ -24,14 +29,73 @@ internal partial class Day16 : IDay
         public readonly string Name;
         public readonly int FlowRate;
         public string[] Tunnels;
-        public List<Valve> ConnectedVales = new();
+        public List<Valve> ConnectedValves = new();
+        public bool Open;
 
         public Valve(string name, int flowrate,  string[] valveList)
         {
             Name = name;
             FlowRate = flowrate;
             Tunnels = valveList;
+            Open = false;
         }
+    }
+
+    class Route
+    {
+        public List<Valve> ValvesOpened;
+        public List<Valve> ValvesVisited;
+        public int Score;
+        public int TimeRemaining;
+
+        public Route ToRoute()
+        {
+            Route route = new()
+            {
+                ValvesOpened = this.ValvesOpened.ToList(),
+                ValvesVisited = this.ValvesVisited.ToList(),
+                TimeRemaining = this.TimeRemaining,
+                Score = this.Score
+            };
+
+            return route;
+        }
+
+        public static bool operator ==(Route a, Route b)
+        {
+            if (a.ValvesOpened.Count != b.ValvesOpened.Count) return false;
+
+            for (int i = 0; i < a.ValvesOpened.Count; i++)
+            {
+                if (a.ValvesOpened[i] != b.ValvesOpened[i]) return false;
+            }
+
+            return true;
+        }
+
+        public static bool operator !=(Route a, Route b) => !(a == b);
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (ValvesOpened.Count != ((Route)obj).ValvesOpened.Count)
+                return false;
+
+            for (int i = 0; i < ValvesOpened.Count; i++)
+            {
+                if (ValvesOpened[i] != ((Route)obj).ValvesOpened[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode() => HashCode.Combine(ValvesOpened);
     }
 
     static List<Valve> ParseInput(string input)
@@ -55,111 +119,58 @@ internal partial class Day16 : IDay
             List<Valve> connectedValves = new();
 
             foreach (string t in v.Tunnels)
-            {
                 connectedValves.Add(valves.Where(v => v.Name == t).First());
-            }
 
-            v.ConnectedVales = connectedValves;
+            v.ConnectedValves = connectedValves;
         }
 
         return valves;
     }
 
-    static bool CompareRoutes(List<Valve> a, List<Valve> b)
+    static int AttemptRoute(Route route, Valve valve)
     {
-        if (a.Count != b.Count) return false;
+        int max = 0;
 
-        for (int i = 0; i < a.Count; i++)
+        if (route.TimeRemaining > 0)
         {
-            if (a[i] != b[i]) return false;
-        }
-
-        return true;
-    }
-
-    static (List<Valve>?, int total, int time) AttemptRoute(Valve currentValve, List<List<Valve>> attemptedRoutes, List<Valve> currentRoute, int total, int time, List<int> openValves)
-    {
-        while (time > 0)
-        {
-            int max = 0;
-            Valve chosenValve = null;
-            foreach (Valve consideration in currentValve!.ConnectedVales)
+            if (!route.ValvesOpened.Contains(valve) && valve.FlowRate > 0)
             {
-                bool alreadyAttemptedRoute = false;
-                foreach (var route in attemptedRoutes)
-                {
-                    if (CompareRoutes(route, currentRoute.Concat(new List<Valve>() { consideration }).ToList()))
-                    {
-                        alreadyAttemptedRoute = true;
-                        break;
-                    }
-                }
-                if (alreadyAttemptedRoute) continue;
+                Route newRoute = route.ToRoute();
+                newRoute.TimeRemaining--;
+                newRoute.ValvesVisited.Add(valve);
+                newRoute.ValvesOpened.Add(valve);
 
-                if (consideration.FlowRate > max || chosenValve == null)
-                {
-                    max = consideration.FlowRate;
-                    chosenValve = consideration;
-                }
+                int score = valve.FlowRate * newRoute.TimeRemaining + AttemptRoute(newRoute, valve);
+                if (score > max) max = score;
             }
 
-            // all rotues already attempted
-            if (chosenValve == null)
+            foreach (Valve v in valve.ConnectedValves)
             {
-                return (null, total, time);
+                if (route.ValvesVisited[^1] == v) continue;
+
+                Route newRoute = route.ToRoute();
+                newRoute.TimeRemaining--;
+                newRoute.ValvesVisited.Add(valve);
+
+                int score = AttemptRoute(newRoute, v);
+                if (score > max) max = score;
             }
-
-
-            currentValve = chosenValve!;
-            time--; // minute travelling
-
-            openValves.Add(currentValve.FlowRate);
-            currentRoute!.Add(currentValve);
-            total += openValves.Sum();
-            time--; // minute opening valve
         }
 
-        attemptedRoutes.Add(currentRoute);
-
-        return (currentRoute!, total, time);
+        return max;
     }
 
     public string SolvePart1(string input)
     {
         var valves = ParseInput(input);
         Valve currentValve = valves.Where(v => v.Name == "AA").First();
-        List<List<Valve>> attemptedRoutes = new();
-        List<Valve> currentRoute = new();
-        List<int> openValves = new();
-        int time = 30;
-        int total = 0;
 
-        int max = 0;
-        while (true)
-        {
-            try
-            {
-                (var route, total, time) = AttemptRoute(currentValve, attemptedRoutes, currentRoute, total, time, openValves);
-                if (total > max) max = total;
-                if (route == null)
-                {
-                    currentRoute.RemoveAt(currentRoute.Count - 1);
-                    attemptedRoutes.Add(currentRoute);
-                }
-                else
-                {
-                    currentRoute = route;
-                    attemptedRoutes.Add(route);
-                }
-                Console.WriteLine(max);
-            }
-            catch (ArgumentOutOfRangeException)
-            {
-                break;
-            }
-        }
+        Route route = new();
+        route.TimeRemaining = 30;
+        route.ValvesOpened = new();
+        route.ValvesVisited = new() { currentValve };
 
-        return $"{max}";
+        return $"{AttemptRoute(route, currentValve)}";
     }
 
     public string SolvePart2(string input)
